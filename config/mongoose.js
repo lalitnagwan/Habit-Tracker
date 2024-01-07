17c2518befcb08acf9e0e42b80272c27ff5037fb
lalitnagwan <141227166+lalitnagwan@users.noreply.github.com>
2024-01-07 16:27:00 +0530

    mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
    try {
       // console.log("dfsfsfs ",process.env.MONGO_URI)
-      const conn = await mongoose.connect(process.env.MONGO_URI);
+      const conn = await mongoose.connect("mongodb+srv://lalitnagwan0:iitspl@5131@cluster0.bpb3miy.mongodb.net/habitTrackerdb");
 
       console.log(`                                 MongoDB Connection Established      \n`);
    } catch (error) {
